4805be88acb7bd587d36c55edf59e565f2ae18b8
JT Jobe <jtjobe@gmail.com>
2014-09-27 13:45:34 -0400

    Exercise 6

--- ex5.rb
+++ ex5.rb
@@ -6,9 +6,14 @@ eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
+height_metric = height * 2.54
+weight_metric = weight * 0.453592
+
 puts "Let's talk about #{name}."
 puts "He's #{height} inches tall."
+puts "He's #{height_metric} centimeters tall."
 puts "He's #{weight} pounds heavy."
+puts "He's #{weight_metric} kilograms."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
