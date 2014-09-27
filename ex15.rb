502e492e029b96da711b351bfaf9b07ba966f041
JT Jobe <jtjobe@gmail.com>
2014-09-27 16:21:44 -0400

    Ex 16

--- ex15.rb
+++ ex15.rb
@@ -1,8 +1,13 @@
+# this sets the filename variable by user input entered in the 
+# command line after the script is called
 filename = ARGV.first
 
+# the "open" command opens the file
 txt = open(filename)
 
+# this print a string telling me the name of the file being run
 puts "Here's your file #{filename}:"
+# this prints the file, after running the "read" command on the file
 print txt.read
 
 print "Type the filename again: "
